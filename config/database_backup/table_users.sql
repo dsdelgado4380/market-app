1b979a30cfef64df954a4f0c478f0ae9a60af1de
Daniel D <dsdelgado.4380@unicesmag.edu.co>
2025-09-02 15:33:50 -0500

    table_users

---
+++ config/database_backup/table_users.sql
@@ -0,0 +1,17 @@
+create table users(
+id BIGSERIAL PRIMARY KEY,
+firstname varchar(30) NOT NULL,
+lastname varchar(30) NOT NULL,
+mobile_number varchar(20) NOT NULL,
+ide_number varchar(15) NULL UNIQUE,
+address TEXT NULL,
+birthday DATE NULL,
+email varchar(200) NOT NULL UNIQUE,
+password TEXT NOT NULL,
+status BOOLEAN NOT NULL DEFAULT TRUE,
+created_at TIMESTAMPTZ NOT NULL DEFAULT now(),
+update_at TIMESTAMPTZ NOT NULL DEFAULT now(),
+deleted_at TIMESTAMPTZ NULL);
+
+INSERT INTO users(firstname,lastname,mobile_number,email,password) 
+VALUES ('Daniel','Delgado','3000456782','dsdelgado@gmail.com','1234');
