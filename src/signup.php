356dce71784b44f442e5089b04e8221b5b9b2fc7
Daniel D <dsdelgado.4380@unicesmag.edu.co>
2025-09-09 15:31:19 -0500

    Creacion Signup.php

---
+++ src/signup.php
@@ -0,0 +1,43 @@
+<?php 
+ //Step 1. Get database access
+ require('../config/database.php');
+
+ //Step 2. Get form-data
+ $f_name      = $_POST['fname'];
+ $l_name      = $_POST['lname'];
+ $m_number    = $_POST['mnumber'];
+ $id_number  = $_POST['idnumber'];
+ $e_mail      = $_POST['email'];
+ $p_wd        = $_POST['passwd'];
+
+ //Step 3. Create query to INSERT INTO 
+ $query ="
+     INSERT INTO users (
+         firstname,
+         lastname,
+         mobile_number,
+         ide_number,
+         email,
+         password
+     ) VALUES (
+     '$f_name',
+     '$l_name',
+     '$m_number',
+     '$id_number',
+     '$e_mail',
+     '$p_wd'
+    )
+";
+
+ //Step 4. Execute query
+ $res=pg_query($conn,$query);
+
+ //Step 5. Validate result 
+ if($res){
+    echo "User has been created successfully !!!";
+ }else{
+    echo "Something wrong!";
+ }
+
+
+?>
