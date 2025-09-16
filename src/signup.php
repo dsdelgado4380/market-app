1180b0765fb241a7e7402b73423f5384295abe33
Daniel D <dsdelgado.4380@unicesmag.edu.co>
2025-09-16 15:35:12 -0500

    Integracion register user with fronted

--- src/signup.php
+++ src/signup.php
@@ -11,35 +11,51 @@
  $p_wd        = $_POST['passwd'];
 
  $enc_pass = password_hash($p_wd,PASSWORD_DEFAULT);
+ //validar datos 
+ $check_email = "
+    SELECT
+        u.email
+    FROM
+        users u
+    WHERE
+        email = '$e_mail' or ide_number ='$id_number'
+    LIMIT 1
+    ";
+    $res_check = pg_query($conn,$check_email);
+    if(pg_num_rows(res_check)> 0){
+        echo "<script>alert('User already exists !!')</script>";
+        header('refresh:0;url=signup.html');
+    } else{
+    //Step 3. Create query to INSERT INTO 
+    $query ="
+        INSERT INTO users (
+            firstname,
+            lastname,
+            mobile_number,
+            ide_number,
+            email,
+            password
+        ) VALUES (
+        '$f_name',
+        '$l_name',
+        '$m_number',
+        '$id_number',
+        '$e_mail',
+        '$enc_pass'
+        )
+    ";
 
- //Step 3. Create query to INSERT INTO 
- $query ="
-     INSERT INTO users (
-         firstname,
-         lastname,
-         mobile_number,
-         ide_number,
-         email,
-         password
-     ) VALUES (
-     '$f_name',
-     '$l_name',
-     '$m_number',
-     '$id_number',
-     '$e_mail',
-     '$enc_pass'
-    )
-";
+    //Step 4. Execute query
+    $res = pg_query($conn,$query);
 
- //Step 4. Execute query
- $res=pg_query($conn,$query);
-
- //Step 5. Validate result 
- if($res){
-    echo "User has been created successfully !!!";
- }else{
-    echo "Something wrong!";
- }
-
+    //Step 5. Validate result 
+    if($res){
+        //echo "User has been created successfully !!!";
+        echo "<script>alert('Success !!! Go to login')</script>";
+        header('refresh:0;url=signin.html');
+    }else{
+        echo "Something wrong!";
+    }
+    }
 
 ?>
